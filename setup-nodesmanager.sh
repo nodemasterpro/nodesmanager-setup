b3e3c05f95bf4b2c835cfd544bed86c8d18ff23f
nodemasterpro <cryptonodesforall@gmail.com>
2024-12-15 20:02:20 +0100

    update

---
+++ setup-nodesmanager.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update system
+apt update && apt upgrade -y
+
+# Install required packages
+apt install git -y
+
+# Remove existing ansible if any
+apt remove ansible -y
+
+# Install ansible properly
+apt-get install -y software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
+
+# Clone and run setup playbook
+git clone https://github.com/nodemasterpro/nodesmanager-setup.git
+cd nodesmanager-setup
+ansible-playbook setup-nodesmanager.yml 
